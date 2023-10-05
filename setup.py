f1691c41e0bc4cc7c43730a0ce8abcc3d7fc2221
Andreas Vishart <45462887+avishart@users.noreply.github.com>
2023-10-05 16:19:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 # Setting up
 setup( 
     name="hpfitter", 
-    version="2.0.5",
+    version="2.0.6",
     author="Andreas Vishart",
     author_email="<alyvi@dtu.dk>",
     description="Hyperparameter fitter for the Gaussian Process from GP-atom",
